c66d0adf74a1197ddd4e0d6728a5cff7930cc83c
Kateryna <1621lika@gmail.com>
2021-01-20 01:56:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,277 @@
+// Task 1.1
+
+// let greeting = "hello";
+// let name1 = "owu";
+// let address1 = "com";
+// let address2 = "ua";
+// let number1 = 1;
+// let number2 = 10;
+// let number3 = -999;
+// let number4 = 123;
+// let number5 = 3.14;
+// let number6 = 2.7;
+// let number7 = 16;
+// let bulean1 = true;
+// let bulean2 = false;
+
+// console.log(greeting);
+// console.log(name1);
+// console.log(address1);
+// console.log(address2);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(number7);
+// console.log(bulean1);
+// console.log(bulean2);
+
+// alert(greeting);
+// alert(name1);
+// alert(address1);
+// alert(address2);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(number7);
+// alert(bulean1);
+// alert(bulean2);
+
+// document.write(greeting);
+// document.write(name1);
+// document.write(address1);
+// document.write(address2);
+// document.write(number1);
+// document.write(number2);
+// document.write(number3);
+// document.write(number4);
+// document.write(number5);
+// document.write(number6);
+// document.write(number7);
+// document.write(bulean1);
+// document.write(bulean2);
+
+// Task 1.2
+
+// greeting = "Привіт!";
+// name1 = "Google";
+// address1 = "www";
+// address2 = "kosetska";
+// number1 = 45;
+// number2 = 22;
+// number3 = -34;
+// number4 = -654;
+// number5 = 5.35;
+// number6 = 7.9;
+// number7 = 16;
+// bulean1 = 2 + 2 === 5;
+// bulean2 = 2 + 2 === 4;
+
+// console.log(greeting);
+// console.log(name1);
+// console.log(address1);
+// console.log(address2);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(number7);
+// console.log(bulean1);
+// console.log(bulean2);
+
+// alert(greeting);
+// alert(name1);
+// alert(address1);
+// alert(address2);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(number7);
+// alert(bulean1);
+// alert(bulean2);
+
+// document.write(greeting);
+// document.write(name1);
+// document.write(address1);
+// document.write(address2);
+// document.write(number1);
+// document.write(number2);
+// document.write(number3);
+// document.write(number4);
+// document.write(number5);
+// document.write(number6);
+// document.write(number7);
+// document.write(bulean1);
+// document.write(bulean2);
+
+// Task 1.3
+
+// const c1 = 65;
+// const c2 = 0;
+// const c3 = 7.5;
+
+// const c4 = "Budmo Gey";
+// const c5 = "Chary Bary";
+// const c6 = "Ni alkogolu";
+
+// console.log(c1);
+// console.log(c2);
+// console.log(c3);
+// console.log(c4);
+// console.log(c5);
+// console.log(c6);
+
+// alert(c1);
+// alert(c2);
+// alert(c3);
+// alert(c4);
+// alert(c5);
+// alert(c6);
+
+// document.write(c1);
+// document.write(c2);
+// document.write(c3);
+// document.write(c4);
+// document.write(c5);
+// document.write(c6);
+
+// Task 1.4
+
+// let lastName = prompt("Type your last name");
+// let firstName = prompt("Type your first name");
+// let secondName = prompt("Type your second name");
+
+// // console.log(lastName, firstName, secondName);
+// // alert(lastName + firstName + secondName);
+// document.write(lastName, firstName, secondName);
+
+// Task 1.5
+
+// let lastName = prompt("Type your last name");
+// let firstName = prompt("Type your first name");
+// let secondName = prompt("Type your second name");
+// let person = lastName + " " + firstName + " " + secondName;
+// console.log(person);
+
+// Task 1.6
+
+// done!
+
+// Task 1.7
+
+// let x1Answer = prompt("type first number");
+// let x2Answer = prompt("type second number");
+// let x3Answer = prompt("type third number");
+
+// let x1 = +x1Answer;
+// let x2 = +x2Answer;
+// let x3 = +x3Answer;
+
+// console.log(x1 + x2 + x3);
+
+//Task 1.8
+
+// let x1Answer = prompt("type first number");
+// let x2Answer = prompt("type second number");
+// let x3Answer = prompt("type third number");
+// let x4Answer = prompt("type fourth number");
+
+// let x1 = parseInt(x1Answer);
+// let x2 = parseInt(x2Answer);
+// let x3 = parseInt(x3Answer);
+// let x4 = parseInt(x4Answer);
+// let result = x1 + x2 + x3 + x4;
+// console.log(result);
+
+// Task 1.9
+
+// let x1Answer = prompt("type first number");
+// let x2Answer = prompt("type second number");
+// let x3Answer = prompt("type third number");
+
+// let x1 = parseFloat(x1Answer);
+// let x2 = parseFloat(x2Answer);
+// let x3 = parseFloat(x3Answer);
+
+// let result = x1 + x2 + x3;
+// console.log(result);
+
+// Task 1.10
+
+// let x1Answer = prompt("type first number");
+// let x2Answer = prompt("type second number");
+// let x3Answer = prompt("type third number");
+
+// let x1 = Math.round(parseFloat(x1Answer));
+// let x2 = Math.round(parseFloat(x2Answer));
+// let x3 = Math.round(parseFloat(x3Answer));
+
+// let result = x1 + x2 + x3;
+// console.log(result);
+
+// Task 1.11
+
+// let x1Answer = prompt("type first number");
+// let x2Answer = prompt("type second number");
+
+// let x1 = parseInt(x1Answer);
+// let x2 = parseInt(x2Answer);
+
+// let result = Math.pow(x1, x2);
+// console.log(result);
+
+// Task 1.12
+
+// let a = 100;
+// let b = "100";
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// let x1 = 5 < 6; //-> true
+// let x2 = 5 == 6; //-> false
+// let x3 = 5 > 6; //-> false
+// let x4 = 5 >= 6; // -> false
+// let x5 = 10 == 10; // -> true
+// let x6 = 10 === 10; // -> true
+// let x7 = 10 < 10; // -> false
+// let x8 = 10 > 10; // -> false
+// let x9 = 10 != 10; // -> false
+// let x10 = 123 === "123"; // -> false
+// let x11 = 123 == "123"; // -> true
+
+// console.log(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11);
+
+//Task Ekstra
+
+// console.log(132 > 100 && 45 < 12 ); //-
+// console.log(34 > 33 && 23 < 90 ); //+
+// console.log(99 > 100 && 45 > 12 ); //-
+// console.log(132 > 100 || 45 < 12 ); //+
+// console.log(111 > 11 || 45 < 111 ); //+
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //+
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); //+
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); //-
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); //+
+// console.log(!!'-1');//+
+// console.log(!!-1);//+
+// console.log(!!'0');//+
+// console.log(!!'null');//+
+// console.log(!!'undefined');//+
+// console.log(!!(3/'owu'));//-
+// console.log((111 > 11 || 45 < 111) ||  !!'0');//+
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));//-
